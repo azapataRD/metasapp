63c8d80f2c10239bfa29fb9f9797c6f5cef8e480
Alejandro Zapata <alejandrojzapata@gmail.com>
2026-04-11 20:31:34 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
